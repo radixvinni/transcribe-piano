4538a6477ca54a5b558fc0a5149362ee251c5e55
radixvinni <radix.vinni@gmail.com>
2016-08-01 00:29:21 +0300

    second_version

--- transcribe2.py
+++ transcribe2.py
@@ -1,69 +1,37 @@
 #%pylab inline
 #pylab.rcParams['figure.figsize'] = 26, 5
+from scipy.io import wavfile
+from scipy.fftpack import fft
+from scipy.optimize import lsq_linear
 import numpy as np
 
 import pylab
-import os, subprocess
+import sys, os, subprocess
+import random
 
 from pyknon.genmidi import Midi
 from pyknon.music import NoteSeq, Note
 
-import librosa
+#learning
+classes = np.arange(-30, 40) #notes from F#2 to E8
+n_classes = len(classes)
+tempo = 180 #tempo of file for sample notes.  fps / tempo gives how much of the sample we analyze
+fps = 6 #frequency of the chords we recognize
+part_length = 44100 / fps #size of a part we analyze
+input_length = 600 #number of amplitudes of spectrogram we analyze
+suppress_noise = 10000 # for nice printing
 
-n_bins = 80
-n_classes = 70
-fmin = librosa.midi_to_hz(36) #C2
-out_tempo = 645 #11 Herz, 8 frames per 1 midi-time
+timespan = 60 * 5 * fps / tempo 
+out_tempo = fps * 60
 
+minimal_volume = 0.01 # output volume threshold 
 
-"""
-def mkt(notes):
-  "create freq samples"
-  midi = Midi(1, tempo=out_tempo)
-  for i in notes: midi.seq_notes([i], time=notes[i])
-  midi.write("temp.mid")
-  subprocess.call("timidity temp.mid -Ow -o temp.wav".split(), stdout=subprocess.PIPE)
-  return read_mp3("temp.wav")
+#testing
+poly = 0 # size of chord to test recognition on
+n_samples = 50 # number of tests
 
-piano_periods = [12,19,24,28, 31, 34]
-first6_periods = [7, 12, 16, 19, 22]
+#todo: polishing
 
-f = read_mp3('sixoctaves.wav')
-period_vol = np.zeros([n_classes, len(piano_periods)])
-for n in range(6):
-  i = f[n+6][143*n:143*(n+1)].argmax()+143*n
-  peak = f[i, n+6]
-  for p in range(len(first6_periods)):
-    period_vol[n][p] = f[i][n+6+first6_periods[p]]/peak
-    f[i][n+6+first6_periods[p]] = -100
-
-for n in range(6,n_classes-6):
-  i = f[n-6][143*n:143*(n+1)].argmax()+143*n
-  peak = f[i, n-6]
-  for p in range(len(piano_periods)):
-    if n-6+piano_periods[p] < n_bins:
-      period_vol[n][p] = f[i][n-6+piano_periods[p]]/peak
-      f[i][n-6+piano_periods[p]] = -100
-
-p = lambda g: librosa.display.specshow(g,sr=44100, fmin=fmin, cmap='spectral') and librosa.display.plt.show()
-"""
-"""
-here is my idea for this algorithm. something like this:
-if there is some time > min_time(pitch)
-and all(f[i] > f[i].mean() during time)?? 
-and f[i-1][time].mean() > f[i-1].mean()
-and f[i+1][time].mean() > f[i+1].mean()
-and f[i+1][time].mean() < f[i][time].mean()??
-and f[i+1][time].mean() < f[i][time].mean()??
-=> there is a note[i] played during time
-
-+ f[i][time] usually descending...
-abs(f[i-1] - f[i+1])[time].mean() < something
-
-TODO rule for period
-
-#librosa.display.specshow(g,sr=44100, x_axis='time', y_axis='cqt_note', fmin=fmin, cmap='spectral');librosa.display.plt.show()
-"""
 def read_mp3(filename):
     if filename.endswith('.mp3'):
       rc=1
@@ -73,27 +41,84 @@ def read_mp3(filename):
       if rc: rc = os.system('mpg321 -w temp.wav '+filename)
       if rc: exit('unable to convert mp3 to wav. install either ffmpeg or avconv or mpg123 or mpg321.')
       filename = "temp.wav"
-    x,fs = librosa.load(filename, sr=44100)
-    return librosa.logamplitude(librosa.cqt(x, sr=fs, fmin=fmin, n_bins=n_bins))
+    return wavfile.read(filename)
 
-f = read_mp3('giovanni_allevi-pensieri_nascosti.mp3')
-
-def find_time(f, i, min_time=80, th=10):
-  mask = (f[i]>th) & (f[i-1]>th) & (f[i+1]>th)
+def channel_freqs(channel1, part_length=part_length, input_length=input_length):
+  #channel1 = channel1[part_length/2:]
+  parts = len(channel1) / part_length
+  freqs = np.array([abs(fft(channel1[i*part_length:(i+1)*part_length]))[:input_length] for i in range(parts)])
+  pylab.imshow(freqs.T, extent=(0,parts,input_length,0), cmap='spectral')
+  #pylab.show()
   
-  mask = np.cumsum(mask)
-  mask[min_time:] = mask[min_time:] - mask[:-min_time]
-  mask = mask==min_time
-  mask = np.cumsum(mask)
-  mask[min_time:] = mask[min_time:] - mask[:-min_time]
-  mask = np.roll(mask > 0, -min_time)
-  #f[i][mask] = -100
-  return np.argwhere(mask^np.roll(mask,1)).reshape(-1,2)
+  return freqs
 
-midi = Midi(1, tempo=out_tempo)
-for i in range(1, n_classes):
-  for start, end in find_time(f, i, 30):
-    midi.seq_notes([Note(i-24, dur=float(end-start)/8/4)], time=start/8)
+def random_samples(sample_size):
+  "get random notes"
+  return np.array([random.sample(range(n_classes), random.choice([poly])) for i in range(sample_size)])
 
-midi.write("output.mid")
-os.system("timidity output.mid")
+def clean_freq(samples):
+  "create freq samples"
+  sample_size = len(samples)
+  chords = [NoteSeq([Note(classes[i]) for i in sample]) for sample in samples]
+  midi = Midi(1, tempo=tempo)
+  for i in range(sample_size): midi.seq_chords([chords[i]], time=5*i)
+  midi.write("temp.mid")
+
+  subprocess.call("timidity temp.mid -Ow -o temp.wav".split(), stdout=subprocess.PIPE)
+
+  rate, data = wavfile.read('temp.wav')
+  return channel_freqs(data.T[0])[:sample_size*timespan:timespan].astype(int) / suppress_noise
+  
+notes_start = clean_freq(np.arange(n_classes).reshape([n_classes,1]))
+
+if poly:
+  answers = random_samples(n_samples)
+  g = clean_freq(answers)
+  k=0
+  for t in range(n_samples):
+    vol_orig = g[t].mean()
+    
+    result = lsq_linear(notes_start.T, g[t], (0, np.inf))
+    notes = result.x.argsort()[-poly:]
+    
+    if set(notes) != set(answers[t]):
+      k+=1
+      print t, 'precision -', set(notes)-set(answers[t]), 'recall +', set(answers[t])-set(notes)
+
+  print k*2, '%  error'
+
+def test_output(x, g):
+  midi = Midi(1, tempo=out_tempo)
+  for i in range(n_classes):
+    dur = 0
+    vol = 0
+    for t,v in enumerate(x.T[i]):
+      min_volume = minimal_volume * g[t] / g.mean()
+      if v*v>min_volume:
+        if dur:
+          vol = (vol / dur + v*v/min_volume ) * (dur+1)
+        else:
+          vol = v*v/min_volume
+        dur += 1
+      elif dur:
+        midi.seq_notes([Note(classes[i], dur=dur/4., volume=min(100,int(vol)))], time=t)
+        dur = 0
+        vol = 0
+  midi.write("output.mid")
+  os.system("timidity output.mid")
+
+#f[fi].argsort()[-3:]
+if not sys.argv[1:]: sys.argv.append('giovanni_allevi-pensieri_nascosti.mp3')
+g = channel_freqs(read_mp3(sys.argv[1])[1].T[0]).astype(int) / suppress_noise
+
+x = np.zeros([len(g),n_classes])
+for i,b in enumerate(g):
+  print '{:.1%}'.format(float(i)/len(g))
+  result = lsq_linear(notes_start.T, b, (0, np.inf))
+  if not result.status:
+    print result
+  x[i] = result.x
+pylab.imshow(x.T, cmap='spectral')
+#pylab.show()
+
+test_output(x, g.mean(axis=1))
